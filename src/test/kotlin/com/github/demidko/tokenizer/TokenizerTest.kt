b55e3e465376d41eac2eac7008ba4ea371aec4a2
Daniil Demidko <demidko@farpost.com>
2021-10-18 19:09:36 +1000

    Актуализировал тесты

--- src/test/kotlin/com/github/demidko/tokenizer/TokenizerTest.kt
+++ src/test/kotlin/com/github/demidko/tokenizer/TokenizerTest.kt
@@ -9,21 +9,9 @@ class TokenizerTest {
 
   enum class Types { MyNumber, MyString, Other }
 
-
   @Test
   fun tokenizeTest() {
-
     val tokens = "475i + '7days' + unknown".tokenize()
-
-    assertThat(
-      tokens.toString(), equalTo(
-        "[Token(lexeme=475, type=DECIMAL_DIGIT_NUMBER), " +
-          "Token(lexeme=i, type=LOWERCASE_LETTER), " +
-          "Token(lexeme=+, type=MATH_SYMBOL), " +
-          "Token(lexeme='7days', type=OTHER_PUNCTUATION), " +
-          "Token(lexeme=+, type=MATH_SYMBOL), " +
-          "Token(lexeme=unknown, type=LOWERCASE_LETTER)]"
-      )
-    )
+    assertThat(tokens, equalTo(listOf("475", "i", "+", "'7days'", "+", "unknown")))
   }
 }
